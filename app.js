aba5b854db2d6594b6191cd44ee5f4bb8a901bb1
Chris <Christopherjorge47@icloud.com>
2024-04-03 13:33:29 -0400

    Added js logic to delete cat images

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 async function getCat(){
 
-    let image = document.querySelector('#cat')
+    let container = document.querySelector('.imgContainer')
+    let image = document.createElement('img')
+    image.setAttribute('class', 'cat')
+    
 
     let response = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_EVIFKqr3kKyNyRunYe4OKiDD8BrFaHdU1KgTRuPBJNIYyr0ktGjz69IQ8dHARLr0`);
+    let data = await response.json();
+    let url = data[0].url
+
+    image.setAttribute('src', url)
+    container.append(image)
+
+}
+
+
+async function getMultipleCats(){
+
+    let container = document.querySelector('.imgContainer')
+
+    let response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=8&api_key=live_EVIFKqr3kKyNyRunYe4OKiDD8BrFaHdU1KgTRuPBJNIYyr0ktGjz69IQ8dHARLr0`);
     let data = await response.json();
     let url = data[0].url
 
@@ -10,17 +27,23 @@ async function getCat(){
 
 }
 
+const removeImages = () => {
+    let container = document.querySelector('.imgContainer')
+    let images = []
+    for(i = 0; i < document.querySelectorAll('.cat').length; i++)
+    {
+        let image = document.querySelectorAll('.cat')[i];
+        images.push(image);
+        // console.log('removing' + image)
+        // image.remove()
+    }
 
-async function getMultipleCats(){
-
-    let image = document.querySelector('#cat')
-
-    let response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&api_key=live_EVIFKqr3kKyNyRunYe4OKiDD8BrFaHdU1KgTRuPBJNIYyr0ktGjz69IQ8dHARLr0`);
-    let data = await response.json();
-    let url = data[0].url
-
-    image.setAttribute('src', url)
-
+    for(i = 0; i < images.length; i++)
+    {
+        let image = images[i];
+        image.remove();
+    }
+    
 }
 
 
