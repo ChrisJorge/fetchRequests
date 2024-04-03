37e393edc23fa270b61740ed4d05a77a3c098987
Chris <Christopherjorge47@icloud.com>
2024-04-03 12:11:51 -0400

    Modified image src to the json data

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
-let apiKey = 'live_EVIFKqr3kKyNyRunYe4OKiDD8BrFaHdU1KgTRuPBJNIYyr0ktGjz69IQ8dHARLr0'
-
 async function getCat(){
 
+    let image = document.querySelector('#cat')
+
     let response = await fetch(`https://api.thecatapi.com/v1/images/search?api_key=live_EVIFKqr3kKyNyRunYe4OKiDD8BrFaHdU1KgTRuPBJNIYyr0ktGjz69IQ8dHARLr0`);
-    let data = response.json();
-    console.log(data.url)
+    let data = await response.json();
+    let url = data[0].url
+
+    image.setAttribute('src', url)
 
 }
+
