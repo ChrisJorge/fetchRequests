f0af3bab1abc1ed9aa558f5d72730e716c3eb5a0
Chris <Christopherjorge47@icloud.com>
2024-04-03 12:18:22 -0400

    Added js logic to get multiple cats

--- app.js
+++ app.js
@@ -11,3 +11,16 @@ async function getCat(){
 }
 
 
+async function getMultipleCats(){
+
+    let image = document.querySelector('#cat')
+
+    let response = await fetch(`https://api.thecatapi.com/v1/images/search?limit=10&api_key=live_EVIFKqr3kKyNyRunYe4OKiDD8BrFaHdU1KgTRuPBJNIYyr0ktGjz69IQ8dHARLr0`);
+    let data = await response.json();
+    let url = data[0].url
+
+    image.setAttribute('src', url)
+
+}
+
+
